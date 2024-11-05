f192050e786725d0a812982c95c65d291549ec9e
Artem Podlesnyy <58227141+underforestaaa@users.noreply.github.com>
2024-11-05 22:20:02 +0300

    Distribution update

--- setup.py
+++ setup.py
@@ -14,10 +14,10 @@ with open('readme.md') as readme_file:
 
 requirements = [
     'numpy>=1.13.3',
+    'cython>=3.0.1',
     'jinja2>=2.9.6',
     'scipy>=1.12.1',
     'scikit-optimize>=0.10.2',
-    'electrode>=1.4',
     'matplotlib>=3.9.2',
     'gdspy>=1.6.13',
     'shapely>=2.0.6',
@@ -30,16 +30,14 @@ short_description = (
 
 setup(
     name='surface-ion',
-    version='1.0.3',
+    version='1.0.4',
     description=short_description,
     long_description=readme,
     long_description_content_type='text/markdown',
     author="Artem Podlesnyy",
     author_email='a.podlesnyy@rqc.ru',
     url='https://github.com/underforestaaa/Sion',
-    packages=find_packages(include=['surface-ion']),
-    package_data={'surface-ion': ['templates/*j2']},
-    include_package_data=True,
+    packages=find_packages(include=['sion']),
     install_requires=requirements,
     license="MIT license",
     keywords=['surface trap', 'ion', 'quantum computing', 'ion simulation',
